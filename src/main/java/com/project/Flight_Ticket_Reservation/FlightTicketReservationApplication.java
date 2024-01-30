2550ac1adf4606df8d143ce8b65147ca49d15f71
kuldeep-ydv88 <kuldeepyadav87336@gmail.com>
2024-01-30 11:42:23 +0530

    Initial commit

---
+++ src/main/java/com/project/Flight_Ticket_Reservation/FlightTicketReservationApplication.java
@@ -0,0 +1,13 @@
+package com.project.Flight_Ticket_Reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightTicketReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightTicketReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/Flight_Ticket_Reservation/FlightTicketReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Flight_Ticket_Reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightTicketReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
