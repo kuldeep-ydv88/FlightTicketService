decf3e1c03c16252967a2beee9a8570a1b657742
kuldeep-ydv88 <kuldeepyadav87336@gmail.com>
2024-02-01 12:02:26 +0530

    add some new flow

--- src/main/java/com/airlines/flight/controller/FlightController.java
+++ src/main/java/com/airlines/flight/controller/FlightController.java
@@ -1,4 +1,30 @@
 package com.airlines.flight.controller;
+import com.airlines.flight.service.FlightService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("flights/")
+@Tag(name = "flight",description = "flight controller APIs")
+@Slf4j
 public class FlightController {
+    @Autowired
+    private FlightService flightService;
+    public void addFlight(){
+
+    }
+
+    public void getFlightInfo(){
+
+    }
+    public void updateFlight(){
+
+    }
+    public void deleteFlight(){
+
+    }
+
 }
--- src/main/java/com/airlines/flight/service/FlightService.java
+++ src/main/java/com/airlines/flight/service/FlightService.java
@@ -1,4 +1,17 @@
 package com.airlines.flight.service;
 
 public class FlightService {
+    public void addFlight(){
+
+    }
+    public void updateFlight(){
+
+    }
+    public void deleteFlight(){
+
+    }
+    public void getFlightId(){
+
+    }
+
 }
