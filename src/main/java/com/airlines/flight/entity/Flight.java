2508e9d1a5ec615382a501818571de12c2229517
kuldeep-ydv88 <kuldeepyadav87336@gmail.com>
2024-02-05 02:35:47 +0530

    Revert "check db"
    This reverts commit ffe8e04f8a29c79afed409a3f442fa2de0188312.

--- src/main/java/com/airlines/flight/entity/Flight.java
+++ src/main/java/com/airlines/flight/entity/Flight.java
@@ -2,7 +2,6 @@ package com.airlines.flight.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Generated;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -12,10 +11,9 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Document(collation = "Flight")
+@Document("flight")
 public class Flight {
     @Id
-    @Generated
     private String id;
     private String flightNumber;
     private String departureCity;
--- src/main/java/com/airlines/flight/repository/FlightRepository.java
+++ src/main/java/com/airlines/flight/repository/FlightRepository.java
@@ -4,5 +4,4 @@ import com.airlines.flight.entity.Flight;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface FlightRepository extends MongoRepository<Flight, String> {
-
 }
